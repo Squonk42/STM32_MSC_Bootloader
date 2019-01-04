a1d37ce281f0d8562f47e15ec9111ac48b61a85b
Michel Stempin <michel.stempin@wanadoo.fr>
2019-01-04 13:54:27 +0100

    Added libopencm3 module

---
+++ libopencm3/lib/efm32/lg/dma_common.d
@@ -0,0 +1,14 @@
+dma_common.o: ../common/dma_common.c \
+ ../../../include/libopencm3/efm32/dma.h \
+ ../../../include/libopencm3/efm32/lg/dma.h \
+ ../../../include/libopencm3/efm32/common/dma_common.h \
+ ../../../include/libopencm3/efm32/memorymap.h \
+ ../../../include/libopencm3/efm32/lg/memorymap.h \
+ ../../../include/libopencm3/cm3/memorymap.h \
+ ../../../include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h
---
+++ libopencm3/lib/lm3s/rcc.d
@@ -0,0 +1,9 @@
+rcc.o: rcc.c /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h ../../include/libopencm3/lm3s/rcc.h \
+ ../../include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ ../../include/libopencm3/cm3/sync.h \
+ ../../include/libopencm3/cm3/common.h
